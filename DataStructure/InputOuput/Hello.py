4f48eaf7cb6287f0b063a3b3edf9671b235544a4
imacoolgirlyo <tjrud1438@naver.com>
2019-02-12 23:08:41 +0900

    stack 입출력 문제

--- DataStructure/InputOuput/Hello.py
+++ DataStructure/InputOuput/Hello.py
@@ -1,2 +1,26 @@
-a, b = map(int, input().split())
-print(a+b)
+# 목표 시간 11시 
+from sys import stdin
+
+stack = []
+for x in range(int(stdin.readline())):
+    arr = stdin.readline().split()
+    if arr[0] == "pop":
+        if stack: print(stack.pop())
+        else :print(-1)
+       
+    elif arr[0] == "push":
+        stack.append(arr[1])
+        
+    elif arr[0]== "top":
+        if stack: print(stack[-1])
+        else: print(-1)
+        
+    
+    elif arr[0]=="size":
+        if stack: print(len(stack))
+        else: print(0)
+    elif arr[0] == "empty":
+        if stack: print(0)
+        else: print(1)
+    else:
+        pass
--- DataStructure/InputOuput/Helloworld.md
+++ DataStructure/InputOuput/Helloworld.md
@@ -1,4 +1,4 @@
-## 백준 입출력 문제 
+## 백준 입출력 문제 및 파이썬 문법
 
 입력 : 첫째 줄에 A, B가 주어짐 
 출력 : 첫째 줄에 A+B 를 출력 
@@ -8,7 +8,7 @@ a, b = map(int, input().split())
 print(a+b)
 ```
 
-### map(function, iterable,...)
+### map(function, iterable,...)ㄴ
 - iterable 한 아이템 모두에 function을 적용하고 결과로 리스트를 반환하는 일을 함
 - 첫번째 인자에는 적용할 메소드, 두번째는 적용할 객체
 > 여기서 **iterable** 하다는 뜻은 한번에 객체 맴버 하나씩 리턴할 수 있는 객체 즉 list, str, tuple 같은 놈들이 iterable 한 객체이다. 반복 가능한 이라는 뜻
@@ -17,3 +17,6 @@ print(a+b)
 
 ## str.split()은 string을 공백으로 나눠주고 리스트 반환
 
+> 응용
+
+첫 줄에 입력 받고, 두번째 줄부터 N개의 줄에는 명령이 하나씩 주어짐
